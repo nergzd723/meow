7e34f2306b8507904c581a43ea9dce7e18342b0f
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 21:31:08 +0300

    Update meow.cpp

--- main/meow.cpp
+++ main/meow.cpp
@@ -11,7 +11,7 @@ int main
     ifstream filetomeow;
     filetomeow.open(argv[1]);
     if (!filetomeow.is_open()){
-        checkforvar(argv[1]);
+        i = checkforvar(argv[1]);
         loopwait();                                 //if nothing worked, enter endless loop of repeating what user typed
         return 0;
     }
