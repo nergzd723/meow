256ffbaa0a16b4c277d00ae8efc0b1e72497a527
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 20:46:46 +0300

    Update meow.cpp

--- main/meow.cpp
+++ main/meow.cpp
@@ -10,14 +10,9 @@ int main
     string buffer;
     ifstream filetomeow;
     filetomeow.open(argv[1]);
-    if (!filetomeow.is_open()){                     
-        if (checkforvar(argv[1]) == 1){ 
-            cout << getenv(argv[1]) << endl;            //if something go wrong, check is it envvar, if it is, read it
-        }
-        else{
-        
-            loopwait();                                 //if nothing worked, enter endless loop of repeating what user typed
-        }
+    if (!filetomeow.is_open()){
+        checkforvar(argv[1]);
+        loopwait();                                 //if nothing worked, enter endless loop of repeating what user typed
         return 0;
     }
     while (getline(filetomeow, buffer)){                //main piece of code that inputs what did it get
