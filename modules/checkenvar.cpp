405e91249602f4e2a0b75153856842ad267d7916
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 21:09:41 +0300

    Update checkenvar.cpp

--- modules/checkenvar.cpp
+++ modules/checkenvar.cpp
@@ -6,8 +6,12 @@ int checkforvar(string main){
     char doll;
     doll << main.at(0);
     if (doll = '$'){
-        cout << getenv(*main);
-        return 1;
+        char* env;
+        env << getenv(main);
+        if (env!=NULL){
+            cout << env << endl;
+        }
+        return 0;
     }
     return 0;
 }
