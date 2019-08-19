79beb71d25c4996afcbfb75597f26bc70acb0b47
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 21:45:59 +0300

    Update meow.cpp

--- main/meow.cpp
+++ main/meow.cpp
@@ -5,16 +5,7 @@
 
 using namespace std;
 
-int checkforvar
-(string& main){
-    char doll;
-    doll << main.at(1);
-    cout << doll;
-    if (doll = '$'){
-        return 0;
-    }
-    return 0;
-    }
+    
 int main
 (int argc, char **argv){
     string buffer;
@@ -22,7 +13,6 @@ int main
     filetomeow.open(argv[1]);
     cout << argv[1];
     if (!filetomeow.is_open()){
-        int i = checkforvar(argv[1]);
         loopwait();                                 //if nothing worked, enter endless loop of repeating what user typed
         return 0;
     }
