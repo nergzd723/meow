9c3b9e72778bc4734842c04f2bfd425ca48838fd
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 20:24:20 +0300

    Update checkforvar.cpp

--- modules/checkforvar.cpp
+++ modules/checkforvar.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include "modulelist.h"
 using namespace std;
 int check(string main){
     char doll;
     doll << str.a(main);
     if (doll = '$'){
         return 1;
-    {
+    }
     return 0;
 }
