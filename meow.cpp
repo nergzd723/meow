61bdec286f75941fb9e96b2a86f785c3b6c0eb6c
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 16:18:56 +0300

    Add files via upload

---
+++ meow.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char **argv){
+    char buff[255]
+    ifstream filetomeow;
+    filetomeow.open(argv[1])
+    if (!filetomeow.is_open()){
+        cout << "No input file available\n";
+        return 1
+    while (getline(filetomeow, buff)){
+        cout << buff << endl;
+}
