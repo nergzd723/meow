e9cbcb564c648ff098c53f47d287c4569e503492
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 21:58:04 +0300

    Update isdir.cpp

--- modules/isdir.cpp
+++ modules/isdir.cpp
@@ -1,12 +1,13 @@
 using namespace std;
 #include <iostream>
 #include <sys/stat.h>
-void main(string dir){
+int main(string dir){
 
   char *myDir = dirname(dir);
   struct stat myStat;
   if ((stat(dir, &myStat) == 0) && (((myStat.st_mode) & S_IFMT) == S_IFDIR)) {
       cout << "is a directory" << endl;
       exit(1)
+      return 0;
   }
 }
