e46faaacb61e99e360d6f50b576ffd1640f1c04b
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 21:17:26 +0300

    Update checkenvar.cpp

--- modules/checkenvar.cpp
+++ modules/checkenvar.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
 #include "modulelist.h"
+
 using namespace std;
+
 int checkforvar(string main){
     char doll;
     doll << main.at(0);
+    
     if (doll = '$'){
         char* env;
-        env << getenv(main);
+        char* v;
+        v* << main;
+        env* << getenv(main);
         if (env!=NULL){
+            
             cout << env << endl;
+            
         }
         return 0;
     }
