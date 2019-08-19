301cb3f9648e3f823a4352b9a30645f424d45444
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-19 16:26:32 +0300

    fix

--- meow.cpp
+++ meow.cpp
@@ -7,10 +7,12 @@ using namespace std;
 int main(int argc, char **argv){
     char buff[255]
     ifstream filetomeow;
-    filetomeow.open(argv[1])
+    filetomeow.open(argv[1]);
     if (!filetomeow.is_open()){
         cout << "No input file available\n";
         return 1
+    }
     while (getline(filetomeow, buff)){
         cout << buff << endl;
+    }
 }
