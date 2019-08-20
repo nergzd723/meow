330819672d58157cbe6afdd5785bff6c037d42f3
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-20 15:51:33 +0300

    Update checkenvar.cpp

--- modules/checkenvar.cpp
+++ modules/checkenvar.cpp
@@ -4,12 +4,17 @@
 
 using namespace std;
 
+void getenv(const string & var){
+    const char* val = ::getenv(var.c_str());
+    cout << val << endl;
+    exit(0)
+  
 int checkforvar(string main){
     char doll;
     doll << main.at(1);
     cout << doll;
     if (doll == '$'){
-        return 1;
+        getenv(main);
     }
     return 0;
 }
