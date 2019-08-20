db15037350cc33dd2e6ec1682add7c5a6b35f6da
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-20 15:46:54 +0300

    Update checkenvar.cpp

--- modules/checkenvar.cpp
+++ modules/checkenvar.cpp
@@ -8,8 +8,8 @@ int checkforvar(string main){
     char doll;
     doll << main.at(1);
     cout << doll;
-    if (doll = '$'){
-        return 0;
+    if (doll == '$'){
+        return 1;
     }
     return 0;
 }
