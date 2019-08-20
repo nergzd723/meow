4e8486d9c1d203e9e31cf1b56c31731e7855ac50
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-20 15:46:15 +0300

    Update isdir.cpp

--- modules/isdir.cpp
+++ modules/isdir.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <filesystem>
 namespace fs = std::filesystem;
-void dirn(std::string dir){
+void dirn(char[] dir){
   if (fs::is_directory(dir)){
     std::cout << "Is a directory" << std::endl;
     exit(0);
