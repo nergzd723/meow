b9bba83e1b95ecfdc8d0e78aeb73eda384ccfdae
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-21 15:09:36 +0300

    Update bark.py

--- main/pytarget/bark.py
+++ main/pytarget/bark.py
@@ -17,9 +17,9 @@ for x in ar:
       if os.path.isdir(ar[i]) and recursive:
         shutil.rmtree(ar[i])
       else:
-        print("bark: Is a directory, ", ar[i])
+        print("bark: Is a directory,", ar[i])
                  
       
   else:
-    print("No such file or directory -", ar[i]) 
+    print("bark: No such file or directory,", ar[i]) 
   i+=1
