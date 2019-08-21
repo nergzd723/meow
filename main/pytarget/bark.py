dd2eed04b6b4464718fe753687791de587d24096
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-21 14:46:10 +0300

    Update bark.py

--- main/pytarget/bark.py
+++ main/pytarget/bark.py
@@ -1,3 +1,13 @@
 import os
 import sys
-print(len(sys.argv), " ", str(sys.argv))
+arg = sys.argv[1:]
+if '-r' in arg:
+  ar = arg[1:]
+  recursive = TRUE;
+else:
+  ar = arg
+for x in ar:
+  if os.path.exists(ar[x]):
+    os.remove(ar[x])
+  else:
+    print("No such file or directory -", ar[x]) 
