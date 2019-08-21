408e17a4d378db8cc0f071d9eeabd60b6e807f0b
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-21 14:30:52 +0300

    Update checkenvar.cpp

--- modules/checkenvar.cpp
+++ modules/checkenvar.cpp
@@ -3,11 +3,12 @@
 
 using namespace std;
 
-void checkforvar(const string & var){
-    const char* val = getenv(var.c_str());
-    if (val == 0){
-        exit(1);
+string checkforvar(const string & key){
+    char* val;
+    val = getenv(key.c_str());
+    string retval = "";
+    if (val != NULL){
+        retval = val;
     }
-    cout << *val << endl;
-    exit(0);
+    return retval;
 }
