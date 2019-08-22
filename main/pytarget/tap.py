0f2664115d4e18bb01536dd8678fae9921945520
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-22 16:35:49 +0000

    Update tap.py

--- main/pytarget/tap.py
+++ main/pytarget/tap.py
@@ -1,12 +1,15 @@
 #Tap for MEOW utilities
 #Copyright Nergzd723
 #LGPL v3
+import datetime
+epoch = datetime.datetime.utcfromtimestamp(0)
 import sys
 import os
 ar = sys.argv[1:]
 file = ar[0]
 try:  
   f = open(file, "r")
+  os.utime(file, (epoch, epoch))
 except:
   f = open(file, "w+")
 try:
