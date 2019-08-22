4abb998cc217cbed09c6748fdd59e4c13c9122b7
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-22 15:16:12 +0000

    Update lt.py

--- main/pytarget/lt.py
+++ main/pytarget/lt.py
@@ -14,6 +14,4 @@ try:
   art = os.listdir(path)
 except:
   print('ln: no such file or directory')
-for file in range(len(art)):
-  print(art[file])
-print(' '.join(str(x) for x in art))
+print('    '.join(str(x) for x in art))
