eb12f95ac58f47c2c1086e92dc7cde62e5411621
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-22 16:54:17 +0000

    Update tap.py

--- main/pytarget/tap.py
+++ main/pytarget/tap.py
@@ -5,11 +5,14 @@ import datetime
 epoch = datetime.datetime.utcfromtimestamp(0)
 import sys
 import os
+import 
 ar = sys.argv[1:]
 file = ar[0]
 try:  
   f = open(file, "r")
-  os.utime(os.path.abspath(file), (epoch, epoch+1))
+  a = os.getcwd()
+  ark = a+'/'+file
+  os.utime(ark, (epoch, epoch+1))
 except:
   print("\n")
   f = open(file, "w+")
