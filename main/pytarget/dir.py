3f74e79559240fef9cdc497db964b308c7bbba5f
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-23 13:57:51 +0300

    Create dir.py

---
+++ main/pytarget/dir.py
@@ -0,0 +1,19 @@
+# Python targeting module for MEOW.
+# Copyright Nergzd723
+# GNU LGPL v3
+import os
+import sys
+ext = False
+ar = sys.argv[1:]
+path = ""
+if not ar:
+  path = os.getcwd()
+else:
+  path = ar[0]
+try: 
+  art = os.listdir(path)
+except:
+  print('lt: no such file or directory')
+
+for file in range(len(art)):
+  print(art[file])
