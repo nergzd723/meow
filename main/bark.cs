3f010bf63faf1acac69582a6b680f96a94c42408
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-26 15:15:20 +0300

    Update bark.cs

--- main/bark.cs
+++ main/bark.cs
@@ -1,107 +1,55 @@
 using System;
 using System.IO;
-namespace Meow
+namespace MeowCS
 {
     class Program
     {
         public static void Main(string[] args)
-
         {
-
             string cwd = Directory.GetCurrentDirectory();
-
             bool recursive = false;
-
             try
-
             {
-
                 if (args[0] == "-r")
-
                 {
-
                     recursive = true;
-
                 }
-
             }
-
             catch
-
             {
-
                 Console.WriteLine("bark: no arguments specified!");
-
             }
-
-
-
             foreach (string p in args)
-
             {
-
                 if (File.Exists(p))
-
                 {
-
                     File.Delete(p);
-
                 }
-
                 else if (Directory.Exists(p))
-
                 {
-
                     if (recursive)
-
                     {
-
                         Directory.Delete(p, true);
-
                     }
-
                     else
-
                     {
-
                         Console.WriteLine("bark: is a directory");
-
                     }
-
                 }
-
-
-
                 else if (File.Exists(cwd + p))
-
                 {
-
                     File.Delete(cwd + p);
-
                 }
-
-
-
                 else if (Directory.Exists(cwd + p))
-
                 {
-
                     if (recursive)
-
                     {
-
                         Directory.Delete(cwd + p, true);
-
                     }
-
                     else
-
                     {
-
                         Console.WriteLine("bark: is a directory");
-
                     }
-
                 }
             }
         }
