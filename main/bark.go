76422e53f68a469cd6f86d4ec8851549a1eee064
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-28 14:07:39 +0300

    Create bark.go

---
+++ main/bark.go
@@ -0,0 +1,15 @@
+#Part of MEOW
+#Basic Auto Remove toolKit
+#By Mark Hargreaves and x0r3d
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+func main(){
+  art = os.Args[1:]
+  fmt.Println("Got arguments", art)
+}
