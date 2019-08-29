3e61e268df04ff35d2767974abbc3d0cb3f875b0
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 15:20:34 +0300

    Update filer.cpp

--- main/filer.cpp
+++ main/filer.cpp
@@ -1,1 +1,30 @@
+#Filer util
+#Creates some directories
+#Testing purpoces only
+#Mark Hargreaves and x0r3d
+#Part of MEOW project
+
+#include <experimental/filesystem>
+#include <string>
+#include <iostream.h>
+
+using namespace std;
+using namespace std::filesystem;
+
+int main
+(int argc, char *argv[])
+{
+  const fs::path workdir = fs::current_path();
+  string dirs;
+  cout << "How many directories to create? ";
+  dirs << cin;
+  int n = stoi(dirs);
+  for (int i; i!=n; i++)
+  {
+    return 0;
+    //not ready yet
+  }
+  
+}
+
 
