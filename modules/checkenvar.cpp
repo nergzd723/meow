664a36fdbcb4320e6ee0134e43414105cfb1af3b
Oleg Sazonov <whitylmn@gmail.com>
2019-09-01 20:04:00 +0300

    Fix checkenvar

--- modules/checkenvar.cpp
+++ modules/checkenvar.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 string checkforvar(const string & key){
+    string readwrite;
+    readwrite = key;
+    if(readwrite.find('$') == 1) readwrite.erase(1, 1);
+    cout << readwrite << endl;
     char* val;
     val = getenv(key.c_str());
     string retval = "";
