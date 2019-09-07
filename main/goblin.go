a9aa8903a91ccb8caf641cde1c42fc14083232c2
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-07 17:47:49 +0300

    GOBLIN: fixes at goblin.go

--- main/goblin.go
+++ main/goblin.go
@@ -1,22 +1,27 @@
 package main
-import(
+
+import (
 	"fmt"
 	"math"
 	"math/rand"
 	"time"
 )
+
 func domath(number float64) {
 	var d = math.Pow(number, 20)
 	var id = math.Pow(d, 20)
 	id++
 }
-func main(){
+func main() {
+	start := time.Now()
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println(rand.Intn(100))
-	for i:=0; i<50000000; i++{
-		var n int = (rand.Intn(10000000))
+	var n int
+	for i := 0; i < 50000000; i++ {
+		n = rand.Intn(10000000)
 		f := float64(n)
 		domath(f)
 	}
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
 
 }
