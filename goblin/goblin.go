f812afc9eb6b8bd943fa2ede607a85a39c50f476
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-08 10:29:49 +0300

    Update goblin.go

--- goblin/goblin.go
+++ goblin/goblin.go
@@ -8,15 +8,17 @@ import (
 )
 
 func domath(number float64) {
-	var d = math.Pow(number, 20)
-	var id = math.Pow(d, 20)
+	var d = math.Pow(number, 15)
+	var id = math.Pow(d, 15)
 	id++
+	fmt.Println(d, id)
+	
 }
 func main() {
 	start := time.Now()
 	rand.Seed(time.Now().UnixNano())
 	var n int
-	for i := 0; i < 50000000; i++ {
+	for i := 0; i < 5000000; i++ {
 		n = rand.Intn(10000000)
 		f := float64(n)
 		domath(f)
