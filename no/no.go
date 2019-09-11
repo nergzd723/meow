4ed03ff89c05af3f0b5be0f0cfc25b1d5ce16fae
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-11 18:34:12 +0300

    Update no.go

--- no/no.go
+++ no/no.go
@@ -4,9 +4,9 @@ import "fmt"
 import "os"
 
 func main(){
-  var arg := os.Args[1:]
-  var a := os.Args[0]
-  for i := 5; i++; i>0{
+  arg := os.Args[1:]
+  a := os.Args[0]
+  for i := 5; i>0; i++{
     fmt.Println(a)
   }
 }
