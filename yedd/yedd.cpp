8caa087c013e92a5b828f2c5abddd516c0ed59ae
Oleg Sazonov <whitylmn@gmail.com>
2019-09-14 21:34:04 +0300

    Upd yedd

--- yedd/yedd.cpp
+++ yedd/yedd.cpp
@@ -9,55 +9,62 @@ using namespace std;
 int main(int argc, char *argv[]) {
     if(argc == 1) {
         cout << "YaDD: Yet another DD" << endl;
-        cout << "Usage: yadd path_from path_to size_of_transaction(in bytes, 4K) count_of_blocks" << endl;
+        cout << "Usage: yadd path_from path_to size_of_transaction(in bytes, by default 4K) count_of_blocks" << endl;
         return 0;
     };
 
+    int bufsize, count;
+
     if(isdigit(atoi(argv[3]))) {
-      const int bufsize = atoi(argv[3]);
+      bufsize = atoi(argv[3]);
     } else {
-      const int bufsize = 4096;
+      bufsize = 4096;
     }
 
     if(isdigit(atoi(argv[4]))) {
-      const int count = atoi(argv[4]);
+      count = atoi(argv[4]);
     } else {
-      const int count = 1;
+      count = 1;
     }
-    char buf[bufsize]; // buffer for readed data
+
+    char buf[bufsize];
 
     try {
-        FILE *read = fopen64(argv[1], "r");
+      FILE* read = fopen64(argv[1],"r");
+      if(!read) {
+        cerr << "read: failed to open by fopen64(), try fopen()";
+        throw 1;
+      }
+    }
+      catch(int a) {
+        FILE* read = fopen(argv[1], "r");
         if(!read) {
-            cerr << "read: Failed to open with fopen64, try fopen()" << endl;
-            throw 1;
-        };
-    } catch(int a) {
-        FILE *read = fopen(argv[1], "r");
-        if(!read) cerr << "read: Failed to open file!" << endl;
-        return 0;
-    };
+          cerr << "read: FAILED to open by fopen(), closing...";
+          return 1;
+        }
+      };
 
-    try {
-        FILE *write = fopen64(argv[2], "w");
+      try {
+        FILE* write = fopen64(argv[2], "w");
         if(!write) {
-            cerr << "write: Failed to open with fopen64, try fopen()" << endl;
-            throw 1;
-        };
-    } catch(int b) {
-        FILE *write = fopen(argv[2], "w");
+          cerr << "write: failed to open by fopen64(), try fopen()";
+          throw 1;
+        }
+      }
+      catch(int a) {
+        FILE* write = fopen(argv[2], "w");
         if(!write) {
-            cerr << "write: Failed to open!" << endl;
-            return 0;
-        };
-    };
- int i;
+          cerr << "write: FAILED to open by fopen64(), closing...";
+          return 1;
+        }
+      };
+
+int i;
+
 while(!feof(read) || count < i) {
   fread(buf, bufsize, 1, read);
   fwrite(buf, bufsize, 1, write);
   ++i;
 }
-
-
     return 0;
 }
