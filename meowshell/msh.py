91160312662c30a653efa77ccc5e51ed49d9859b
Mark <clashclanacc2602@gmail.com>
2019-09-15 18:01:01 +0300

    moving msh

--- meowshell/msh.py
+++ meowshell/msh.py
@@ -1,32 +1,9 @@
 import sys
 import os
 import platform
+from mshh import cd, ls, lexer
 import mshh
 from importlib import reload
-
-def cd(comm):
-    try:
-        os.chdir(comm)
-    except:
-        try:
-            os.chdir(Vars.cwd+comm)
-        except:
-            print("No such file or directory", comm)
-def ls(void):
-    m = mshh.Vars
-    lt = os.listdir(m.cwd)
-    for entry in lt:
-        print(entry)
-    return
-def lexer(comm, args):
-    availmblecomms = ['cd', 'ls']
-    if comm not in availmblecomms:
-        print("No such file or directory or command name")
-    else:
-        i = availmblecomms.index(comm)
-        exec(availmblecomms[i]+"({})".format('"'+args+'"'))
-        return
-    return
 
 def mainstream():
     reload(mshh)
@@ -42,7 +19,7 @@ def mainstream():
             arg = ""
         if l == 0:
             continue
-        arg = comms[l]+arg
+        arg = arg+" "+comms[l]
     comd = comms[0]
     lexer(comd, arg)
 if __name__ == "__main__":
