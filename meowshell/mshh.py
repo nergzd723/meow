d827d538b012425b6a6a81dc2dcabb5623d962d1
Mark <clashclanacc2602@gmail.com>
2019-09-15 18:01:08 +0300

    fix

--- meowshell/mshh.py
+++ meowshell/mshh.py
@@ -6,4 +6,26 @@ class Vars:
     usrbincomm = os.listdir(cwd)
     localcomm = os.listdir(cwd)
     fullpath = localcomm+usrbincomm
-
+def cd(comm):
+    try:
+        os.chdir(comm)
+    except:
+        try:
+            os.chdir(Vars.cwd+comm)
+        except:
+            print("No such file or directory", comm)
+def ls(void):
+    m = Vars
+    lt = os.listdir(m.cwd)
+    for entry in lt:
+        print(entry)
+    return
+def lexer(comm, args):
+    availmblecomms = ['cd', 'ls']
+    if comm not in availmblecomms:
+        print("No such file or directory or command name")
+    else:
+        i = availmblecomms.index(comm)
+        exec(availmblecomms[i]+"({})".format('"'+args+'"'))
+        return
+    return
