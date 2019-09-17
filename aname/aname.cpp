caad75735c5c1669bdecfc4ca9c638e356c9fddf
Oleg Sazonov <whitylmn@gmail.com>
2019-09-17 20:14:15 +0300

    Some fooling detected!

--- aname/aname.cpp
+++ aname/aname.cpp
@@ -2,12 +2,17 @@
 // AName: uname analog
 // Copyright 2019, Oleg Sazonov(@x0r3d) in collaboration with Mark Hargreaves(@nergzd723)
 // 12.09.19: Initial write
+// 17.09.19: Rewrite to working state
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << system("uname -a");
+    ifstream cat("/proc/version");
+    string out;
+    while (getline(cat, out)) {
+        cout << out << endl;
+    }
     return 0;
 }
