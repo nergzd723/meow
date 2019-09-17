abf9132079386a81d0c8187d376340e662815f34
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-17 20:31:26 +0300

    Create ilclasses.py

---
+++ dutils/ilclasses.py
@@ -0,0 +1,23 @@
+# I love classes or how to Hello World in 20 lines
+class TransmitVeryImportantmsg:
+  def enc(self):
+    m = 'Hello World'
+    result = ''
+    return result
+class Main:
+  def printl(self, arg):
+    print(arg)
+class Hello:
+  def __init__(self):
+    m = Main()
+    t = TransmitVeryImportantmsg()
+    n = t.enc()
+    m.printl(n)
+if __name__ == "__main__":
+  try:
+    h = Hello()
+  except:
+    class Error:
+      def __init__(self):
+        print('Lol you just screwed up your Hello World Program\n Who might even thought')
+    e = Error()
