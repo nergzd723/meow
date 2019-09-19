0c7bfe956fe11c349dcf7f7e835a80e7d28eb2aa
Oleg Sazonov <whitylmn@gmail.com>
2019-09-19 20:55:45 +0300

    Hello, Markъ!

--- dutils/ilclasses.cpp
+++ dutils/ilclasses.cpp
@@ -1,3 +1,3 @@
 #include <iostream>
-using namespace std;int main(void) {struct hello {string m = "Hello World";string out = m; };class h {public: int i = 0;struct hello fuck;h(void) {};void print(void) {cout << fuck.out << endl;}};class e : public h {public: e(void) {i++;};};class l : public e {public: l(void) {i++;};};class o : public l {public: o(void) {i++;};};o hell;hell.print();return 0;
+using namespace std;int main(void) {struct hello {string m = "Hello Markъ";string out = m; };class h {public: int i = 0;struct hello fuck;h(void) {};void print(void) {cout << fuck.out << endl;}};class e : public h {public: e(void) {i++;};};class l : public e {public: l(void) {i++;};};class o : public l {public: o(void) {i++;};};o hell;hell.print();return 0;
 }
