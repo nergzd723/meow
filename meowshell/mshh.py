7c632fc0df42e8eae59d3659bc70c0f9276abc5e
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-19 20:03:29 +0300

    Update mshh.py

--- meowshell/mshh.py
+++ meowshell/mshh.py
@@ -1,11 +1,20 @@
 import os
 import platform
+lastcomm = ""
+lastarg = ""
+availmblecomms = ['cd', 'rabbid', '^[[A']
 class Vars:
     cwd = os.getcwd()
     pcname = platform.node()
     usrbincomm = os.listdir(cwd)
     localcomm = os.listdir(cwd)
     fullpath = localcomm+usrbincomm
+def arrowp(void):
+    if lastcomm[-1:] == ')':
+        exec(lastcomm+"({})".format('"'+lastarg+'"'))
+        return
+    else:
+        os.system(lastcomm+" "+lastargs)
 def cd(comm):
     try:
         os.chdir(comm)
@@ -24,14 +33,20 @@ def rabbid(void):
     print("Yay! You find an easter egg!\nmsh brought to you by Mark Hargreaves, 19 years old blue haired MGU student from Moscow. Enjoy!")
     return
 def lexer(comm, args):
-    availmblecomms = ['cd', 'rabbid']
     if comm not in availmblecomms:
         try:
             os.system(comm+" "+args)
         except:
             print("No such file or directory or command name")
+    
     else:
         i = availmblecomms.index(comm)
+        if comm == '^[[A':
+            availmblecomms[i] = 'arrowp'
         exec(availmblecomms[i]+"({})".format('"'+args+'"'))
+        lastcomm = availmblecomms[i]
+        lastarg
         return
+    lastcomm = comm
+    lastarg = args
     return
