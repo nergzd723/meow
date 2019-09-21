aafd5ad1e658bd45ca76a11e2cca033a69f21c15
Oleg Sazonov <whitylmn@gmail.com>
2019-09-21 20:44:47 +0300

    It's alive!

--- vkutils/liker.cpp
+++ vkutils/liker.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "lib/api.h"
+#include "lib/json.hpp"
 
 using namespace std;
+using  json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
     if (argc == 1 || argc > 4) {
         cout << "Liker: VK Photo Liker" << endl;
-        cout << "Usage: liker login pass album" << endl;
+        cout << "Usage: liker login pass album_owner_id album_id" << endl;
         return 0;
     }
 
@@ -19,5 +21,14 @@ int main(int argc, char *argv[]) {
         cout << "auth ok!" << endl;
         cout << "Access token: " << client.access_token() << endl;
     }
+
+    VK::params_map test = {
+    {"owner_id", "223134400"},
+    {"album_id", "saved"},
+    {"count", "2"}
+    };
+   json parse = client.call("photos.get", test);
+   auto count = parse["response"]["count"].get<int>();
+   cout << count << endl;
     return 0;
 }
