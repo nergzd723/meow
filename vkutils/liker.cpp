191b6a8711495c6d0f77f2bcdc2a96f3034ebb99
Oleg Sazonov <whitylmn@gmail.com>
2019-09-21 18:38:40 +0300

    Making prototype working!

--- vkutils/liker.cpp
+++ vkutils/liker.cpp
@@ -4,8 +4,20 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
+    if (argc == 1 || argc > 4) {
         cout << "Liker: VK Photo Liker" << endl;
+        cout << "Usage: liker login pass album" << endl;
         return 0;
     }
+
+    string login,pass;
+    login = argv[1];
+    pass = argv[2];
+
+    VK::Client client;
+    if(client.auth(login, pass)) {
+        cout << "auth ok!" << endl;
+        cout << "Access token: " << client.access_token() << endl;
+    }
+    return 0;
 }
