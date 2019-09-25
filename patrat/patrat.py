6aa72ada4306406b5cd1864c38776782b895840f
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-25 18:26:25 +0300

    Update patrat.py

--- patrat/patrat.py
+++ patrat/patrat.py
@@ -36,7 +36,7 @@ def tarball(patmit):
 #unzips tar patmit in the tempdir
 def detar(patmit):
     os.system("cp {} {} > /dev/null".format(cwd+"/"+PATRAT_PATRAT+PATRAT_PATMIT+patmit+"/"+patmit+".pat", cwd+"/"+"RAT"))
-    os.system("tar -xvf RAT > /dev/null")
+    os.system("tar -xzf {} > /dev/null".format(cwd+"/"+"RAT"))
 
 #generates name for patmit, 5 symbols
 def genpatmitname():
