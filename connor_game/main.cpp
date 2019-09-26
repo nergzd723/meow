459a5997c2e054f85c514222d2506014c1e28b68
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-26 17:20:19 +0300

    It`s so working that it`s not

--- connor_game/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Добро пожаловать в игру по мотивам допроса '28 ударов ножей' Detroit: Become human!" << endl;  
-    return 0;
-}
