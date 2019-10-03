89ac0a9f2056131a7374f92705c2e3a00e334cf8
Oleg Sazonov <osaz1423@gmail.com>
2019-10-03 20:27:55 +0300

    format bark

--- bark/bark.cpp
+++ bark/bark.cpp
@@ -4,64 +4,65 @@
 // 25.08.19: Inital write
 // 26.08.19: Moved to Boost library
 // 20.09.19: Fix for one file
-#include <iostream> // for IO management
-#include <cstring> // for strcmp
 #include <boost/filesystem.hpp> // for filesystem management
+#include <cstring>              // for strcmp
+#include <iostream>             // for IO management
 using namespace std;
 using namespace boost::filesystem;
 int main(int argc, char *argv[]) {
 
-    bool Recursive = false; // bool for -r key
+  bool Recursive = false; // bool for -r key
 
-    if(argc == 1) { // if no arguments, write help
-        cout << "Basic Auto Remove toolKit" << endl;
-        cout << "Usage: bark [OPTIONS] paths_to_remove" << endl;
-        cout << "Options:" << endl;
-        cout << "-r                                 detete 'em recursivily" << endl;
-        return 0;
-    };
-    if(!strcmp(argv[1], "-r")) { // check for -r key
+  if (argc == 1) { // if no arguments, write help
+    cout << "Basic Auto Remove toolKit" << endl;
+    cout << "Usage: bark [OPTIONS] paths_to_remove" << endl;
+    cout << "Options:" << endl;
+    cout << "-r                                 detete 'em recursivily" << endl;
+    return 0;
+  };
+  if (!strcmp(argv[1], "-r")) { // check for -r key
     Recursive = true;
-    };
+  };
 
-    path need_to_remove;
-    if(!Recursive) {
-        need_to_remove = argv[1];
+  path need_to_remove;
+  if (!Recursive) {
+    need_to_remove = argv[1];
+  } else {
+    need_to_remove = argv[2];
+  }
+
+  if (is_directory(need_to_remove) == true &&
+      Recursive == false) { // checks for key and directory comparation
+    cout << "Is a directory(use -r to delete 'em recursivily" << endl;
+    return 1;
+  } else if (is_directory(need_to_remove) == true && Recursive == true) {
+    uintmax_t count = remove_all(need_to_remove);
+    if (count == -1) {
+      cout << "Failed to remove directory!" << endl;
+      return 1;
     } else {
-        need_to_remove = argv[2];
-    }
-
-
-    if(is_directory(need_to_remove) == true && Recursive == false) { // checks for key and directory comparation
-        cout << "Is a directory(use -r to delete 'em recursivily" << endl;
-        return 1;
-    } else if(is_directory(need_to_remove) == true && Recursive == true) {
-       uintmax_t count = remove_all(need_to_remove);
-       if(count == -1) {
-           cout << "Failed to remove directory!" << endl;
-           return 1;
-       } else {
-           cout << "Removed " << count << " files and directories!" << endl;
-           return 0;
-       };
+      cout << "Removed " << count << " files and directories!" << endl;
+      return 0;
     };
-    if(is_directory(need_to_remove) == false) {
-        if(strcmp(argv[1], "-r")  && argc > 2) { // check for key, and arguments
-        int i;
-        for(i = 1;i < argc;i++) { // deleting multiple files
-            path removed = argv[i];
-            remove(removed);
-        }
-        cout << "Removed" << i << "files!" << endl; // write how many files has been deleted
-        return 0;
-        }
-        if(!remove(need_to_remove)) {
-            cout << "Failed to remove!" << endl;
-            return 1;
-        } else {
-            cout << "Removed!" << endl;
-            return 0;
-        };
-    }; // main part end
-   return 0;
+  };
+  if (is_directory(need_to_remove) == false) {
+    if (strcmp(argv[1], "-r") && argc > 2) { // check for key, and arguments
+      int i;
+      for (i = 1; i < argc; i++) { // deleting multiple files
+        path removed = argv[i];
+        remove(removed);
+      }
+      cout << "Removed" << i << "files!"
+           << endl; // write how many files has been deleted
+      return 0;
+    }
+    if (!remove(need_to_remove)) {
+      cout << "Failed to remove!" << endl;
+      return 1;
+    } else {
+      cout << "Removed!" << endl;
+      return 0;
+    };
+  }; // main part end
+  return 0;
 };
