5ba75e504ac98f6ed475871147399e20eaa64437
Mark <clashclanacc2602@gmail.com>
2019-10-05 14:35:46 +0300

    fix patratgit, maybe it`s working, IDK

--- patrat/patratgit.py
+++ patrat/patratgit.py
@@ -14,58 +14,50 @@ import time
 #init
 arg = sys.argv[1:]
 cwd = os.getcwd()
-PATRAT_PATRAT = patrat.searchpokemon()+'/'
+PATRAT_PATRAT = patrat.searchpokemon()
 PATRAT_PATMIT = PATRAT_PATRAT+"patmit/"
 PATRAT_GIT = ".patrat/patrat-git/"
 PATRAT_GITFILE = PATRAT_PATRAT+"PATRAT-GIT"
 PATRAT_LIST = patrat.getpatmitlist()
 PATRAT_LPAT = PATRAT_LIST[len(PATRAT_LIST)-1]
 PATRAT_MERGESTARTPOINT = PATRAT_LIST[0]
-PATRAT_HEAD = PATRAT_PATRAT+"HEADofGIT"
+PATRAT_HEAD = PATRAT_PATRAT+"GITHEAD"
+PATRAT_PATLIST = patrat.gettimelist()
+PATRAT_MSGLIST = patrat.getmsglist()
 
 #main part
-
-#enables patrat-git via empty files
-def patratgitenable():
-    patrat.patlogger("patrat-git enable: init")
-    if os.path.exists(PATRAT_GITFILE):
-        patrat.reporterr("patrat-git support is present")
-    os.mkdir(PATRAT_GIT)
-    os.mkdir(PATRAT_PATMIT+"PGIT/")
-    open(PATRAT_GITFILE, "w+")
-    patrat.patlogger("done enabling")
-    print("patrat-git support enabled")
-
-
-#moves specifical patmit to PATRAT_GIT
-def patratgitrefresh(patmit):
-    patrat.patlogger("patrat-git refresh: detarring patmit "+patmit+" to "+PATRAT_GIT)
-    patrat.syscall("cp {} {} > /dev/null".format(cwd+"/"+".patrat/patmit/"+patmit+"/"+patmit+".pat", cwd+"/"+PATRAT_GIT+"TEMPD"))
-    patrat.syscall("cd {} && tar -xzf TEMPD > /dev/null && rm TEMPD".format(PATRAT_GIT))
-    patrat.patlogger("patrat-git refresh: detarred to "+PATRAT_GIT+" patmit "+patmit)
-    patrat.patlogger("patrat-git refresh: ---------------------------------------")
-
-#moves HB to the PATRAT_GIT and ready a repository
-def patratgitinit():
-    if not os.path.exists(patrat.PATRAT_PATRAT):
-        patrat.reporterr("Tried to patrat-git init without patrat repository")
-    if not os.path.exists(PATRAT_GITFILE):
-        patrat.reporterr("Repository init without patrat-git support")
-    o = open(PATRAT_HEAD, "w+")
-    o.write(PATRAT_LIST[0])
-    o.close()
-    patratgitsetup()
-    #patratgitrefresh("HOTB")
-    patrat.patlogger("patratgitinit: init git repository")
-    #patrat.syscall("cd {} && git init".format(PATRAT_GIT))
 
 #exec git code
 def patratgitexec(call):
     patrat.patlogger("patratgitexec: got a call "+call)
     patrat.syscall("cd {} && {}".format(PATRAT_GIT, call))
 
+def updhead(patmit):
+    p = open(PATRAT_HEAD, "w")
+    p.write(patmit+" ")
+    p.close()
+    patrat.patlogger("HEAD is now on patmit "+patmit)
+
+#setup patrat-git
+def patratgitsetup():
+    if os.path.exists(PATRAT_GITFILE):
+        patrat.reporterr("patrat-git support present")
+    o = open(PATRAT_GITFILE, "w+")
+    o.close()
+    p = open(PATRAT_HEAD, "w+")
+    p.write(PATRAT_LIST[0])
+    p.close()
+    print("PATRAT-GIT setup util\nThis util is used to merge patmits to already init git repositorys")
+    origin = input("Enter origin git repository(e.g https://github.com/meowproj/patrat) ")
+    patrat.patlogger("patratgitsetup: Cloning repo "+origin+" to patrat-git folder")
+    os.mkdir(PATRAT_GIT)
+    patrat.syscall("cd "+PATRAT_PATRAT+" && git clone "+origin+" patrat-git")
+    print("Cloned origin to "+PATRAT_GIT)
+    print("Patrat-git ready to work")
+
 #opens MEOWSHELL seance at PATRAT_GIT folder
 def patratgitshell():
+    os.chdir(PATRAT_GIT)
     sep = " "
     patrat.patlogger("patratgitshell: start stream")
     while True:
@@ -77,62 +69,26 @@ def patratgitshell():
             patrat.patlogger(sep.join(n))
             os.system(sep.join(n))
 
-def patratgitcommitpatmit(patmit):
-    patrat.patlogger("patratgitcommitpatmit: got a task: push "+patmit+" to git repository and commit")
-    patratgitrefresh(patmit)
-    patratgitexec('git add . && git commit -m "Automatically created patmit {}"'.format(patmit))
-    patrat.patlogger("patratgitcommitpatmit: done for patmit "+patmit)
-
-#patratgit setup: setup repo origin and another
-def patratgitsetup():
-    print("patratgit setup utility\nYou can always change settings later")
-    origin = input("Origin of your repository, it`s remote(e.g. https://github.com/patrat/patrat-repo)")
-    patrat.patlogger("patratgitsetup: got from user: "+origin+" repo")
-    patratgitexec("git clone {}".format(origin))
-    print("Done setup!\nOrigin = "+origin)
-
-def patratgitmerge():
-    H = open(PATRAT_HEAD, "r")
-    HEAD = H.read()
-    H.close()
-    r = open(PATRAT_HEAD, "w")
-    PATRAT_MERGESTARTPOINT = HEAD
-    print("Starting merge to GIT")
-    patrat.patlogger("patratgitmerge: Starting merge, starting point: "+PATRAT_MERGESTARTPOINT)
+def patratgitmergeall():
     for patmit in PATRAT_LIST:
-        patratgitcommitpatmit(patmit)
-        r.write(patmit)
-        patrat.patlogger("Merged patmit "+patmit)
-    print("Done merging to GIT, current HEAD = "+PATRAT_LPAT)
-
-#doing patmit of current changes, refresh a patratgit, git commit and git push
-#def patratgitapply():
-  #  patratgitexec("git pull")
- #   patrat.patlogger("patratgitapply: creating tarball of PGIT")
-#    patrat.tarball("PGIT")
-   # patrat.patlogger("patratgitapply: refresh")
-  #  patratgitrefresh("PGIT")
- #   msg = input("Enter git commit message ")
-#    patratgitexec("git add .")
-    #patrat.patlogger("patratgitapply: creating commit with message "+msg)
-   # patratgitexec('git commit -m "{} (patratgit)"'.format(msg))
-  #  patratgitexec("git push origin master")
- #   print("Done! If something is wrong, you can always use patratgit shell or watch DLOG for errors")
-#    patrat.patlogger("patratgitapply: success")
+        i = PATRAT_LIST.index(patmit)
+        syscall("cp {} {} > /dev/null".format(cwd+"/"+PATRAT_PATRAT+PATRAT_PATMIT+patmit+"/"+patmit+".pat", PATRAT_GIT+"GITRAT"))
+        print("Copy patmit "+patmit)
+        syscall("cd {} && tar -xzf {} > /dev/null".format(PATRAT_GIT, "GITRAT"))
+        print("Detar patmit "+patmit)
+        patratgitexec("git add .")
+        patratgitexec("git commit -m {}".format(PATRAT_MSGLIST[i]))
+        print("Commit patmit "+patmit+" with message "+PATRAT_MSGLIST[i])
+        updhead(patmit)
 
 def lex():
-    avcomm = ['enable', 'init', 'refresh', 'shell', 'setup', 'apply', 'merge']
+    avcomm = ['setup', 'mergeall', 'refresh', 'shell', 'setup', 'apply', 'merge']
     if arg[0] in avcomm:
-        if arg[0] == 'enable':
-            patratgitenable()
-        if arg[0] == 'init':
-            patratgitinit()
-        if arg[0] == 'refresh':
-            patmit = arg[1]
-            patratgitrefresh(patmit)
+        if arg[0] == 'setup':
+            patratgitsetup()
         if arg[0] == 'shell':
             patratgitshell()
-        if arg[0] == 'merge':
-            patratgitmerge()
+        if arg[0] == 'mergeall':
+            patratgitmergeall()
 if __name__ == "__main__":
     lex()
