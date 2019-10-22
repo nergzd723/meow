271551aa324b7b51bbd7916d6e039f0598bf01dd
Mark <clashclanacc2602@gmail.com>
2019-10-22 20:48:44 +0300

    something like that

---
+++ patrat/patcatlegacy.py
@@ -0,0 +1,19 @@
+# PATRAT legacy mode module
+# makes 10 patmits
+# patrat loadmod patrat.py --legacy
+
+#if blocks and other that require : and indents will not work
+
+# it`s simple as hell
+
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
+patmit("HAHAHA")
