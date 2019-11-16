8c00ab790eeae70c5aed5f4e1d65c5f594c21cda
Mark <clashclanacc2602@gmail.com>
2019-11-16 22:13:38 +0200

    i bet it worked

--- patrat/charmander.py
+++ patrat/charmander.py
@@ -1,6 +1,14 @@
 #charmander crypting util for patrat
 #use char -char to test
+import sys
+import os
 
+#os.system call and log
+def syscall(call):
+    patlogger(call)
+    out = subprocess.check_output(call, shell=True) #shell=False due to security issue
+    patlogger(out)
+    
 CHAR_ARGS = sys.argv[1:]
 CHAR_CHAR = '.char/'
 CHAR_FILES = [ f for f in os.listdir('.') if os.path.isfile(os.path.join('.',f)) ]
