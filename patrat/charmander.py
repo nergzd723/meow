f8266ef1bd1bc18ecbd7bc5249a6fc2bdeeec667
Mark <clashclanacc2602@gmail.com>
2019-11-17 10:50:48 +0200

    fix?

--- patrat/charmander.py
+++ patrat/charmander.py
@@ -14,9 +14,9 @@ CHAR_FILES = [ f for f in os.listdir('.') if os.path.isfile(os.path.join('.',f))
 CHAR_BLOB = CHAR_CHAR+'obj/'
 #encrypt to blob
 def charenc(filen, passc, pathto):
-    syscall("gpg --output {} --symmetric --passphrase {} --batch -c {}".format(pathto, passc, filen))
+    syscall("gpg --output {} --symmetric --passphrase {} --batch -c {}".format('"'+pathto+'"', passc, '"'+filen+'"'))
 
 #decrypt blob
 def chardec(filen, passc, pathto):
-    syscall("gpg --output {} --passphrase {} --batch {}".format(pathto, passc, filen))
+    syscall("gpg --output {} --passphrase {} --batch {}".format('"'+pathto+'"', passc, '"'+filen+'"'))
 
