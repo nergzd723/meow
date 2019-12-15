d4b2d28343915ae9437d40b043d49e72e47ac74f
Mark <clashclanacc2602@gmail.com>
2019-12-15 10:49:19 +0200

    add href and button

--- endure/endure.py
+++ endure/endure.py
@@ -2,6 +2,7 @@
 import sys
 from os import getcwd
 from termcolor import cprint
+from random import randint
 ENDURE_CV = 1
 ENDURE_ARGS = sys.argv[1:]
 ENDURE_DOC = ""
@@ -50,19 +51,22 @@ def header(*arg):
     align = 'left'
     size = "1"
     color = "black"
+    ide = randint(100000000, 500000000)
     if arlen > 1:
         size = arg[1]
     if arlen > 2:
         align = arg[2]
     if arlen > 3:
         color = arg[3]
+    if arlen > 4:
+        ide = arg[4]
     if size > 8 or size < 1:
         cc_err("bad <h> size: "+str(int(size)))
     if align == 'left' or align == 'right' or align == 'center':
         pass
     else:
         cc_err("bad align: "+align)
-    ENDURE_BODYTEMP = ENDURE_BODYTEMP + '<h{} align={} style="color:{}">{}</h{}>\n'.format(size, align, color, text, size)
+    ENDURE_BODYTEMP = ENDURE_BODYTEMP + '<h{} align={} style="color:{}" id={}>{}</h{}>\n'.format(size, align, color, ide, text, size)
 def background(cl):
     global ENDURE_BODYBACK
     ENDURE_BODYBACK = cl
@@ -72,17 +76,54 @@ def paragraph(*arg):
     align = "left"
     text = arg[0]
     color = "black"
+    ide = randint(100000000, 500000000)
     if arlen > 1:
         align = arg[1]
     if arlen > 2:
         color = arg[2]
-    ENDURE_BODYTEMP = ENDURE_BODYTEMP + '<p align={} style="color:{}">'.format(align, color)+text+"</p>"+backslashn
+    if arlen > 3:
+        ide = arg[3]
+    if align == 'left' or align == 'right' or align == 'center':
+        pass
+    else:
+        cc_err("bad align: "+align)
+    ENDURE_BODYTEMP = ENDURE_BODYTEMP + '<p align={} style="color:{}" id={}}>'.format(align, color, ide)+text+"</p>"+backslashn
 def title(hdr):
     global ENDURE_HEADTEMP
     ENDURE_HEADTEMP = ENDURE_HEADTEMP + "<title>"+hdr+"</title>\n"
+def button(*arg):
+    global ENDURE_BODYTEMP
+    action = "ffff"
+    align = "left"
+    arlen = len(arg)
+    text = arg[0]
+    if arlen > 1:
+        action = arg[1]
+    if arlen > 2:
+        align = arg[2]
+    if align == 'left' or align == 'right' or align == 'center':
+        pass
+    else:
+        cc_err("bad align: "+align)
+    ENDURE_BODYTEMP = ENDURE_BODYTEMP + "<button align={}".format(align)
+    if not action == "ffff":
+        ENDURE_BODYTEMP = ENDURE_BODYTEMP + 'onclick="{}">{}</button>\n'.format(action, text)
+    else:
+        ENDURE_BODYTEMP = ENDURE_BODYTEMP + '>{}</button>\n'.format(text)
 def html(code):
     global ENDURE_OTHERTEMP
     ENDURE_OTHERTEMP = ENDURE_OTHERTEMP + code
+def href(*a):
+    global ENDURE_BODYTEMP
+    alen = len(a)
+    text = "ffff"
+    hreff = a[0]
+    if alen > 1:
+        text = a[1]
+    if not text == "ffff":
+        ENDURE_BODYTEMP = ENDURE_BODYTEMP + '<p><a href="{}">{}</a></p>'.format(hreff, text)
+    else:
+        ENDURE_BODYTEMP = ENDURE_BODYTEMP + '<p><a href="{}"></a></p>'.format(hreff)
 def dino():
     global whereto
     if "-o" in ENDURE_ARGS:
