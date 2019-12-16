4ddb4dc6527ecc1db4306373f58bbc93dc95bbc2
Mark <clashclanacc2602@gmail.com>
2019-12-16 21:03:50 +0200

    fix

--- endure/hello.js
+++ endure/hello.js
@@ -1,11 +1,9 @@
 function world()
 {
-    let fuck = document.getElementById("fuck")
     let fuck1 = document.getElementById("fuck1")
     let fuck2 = document.getElementById("fuck2")
     let fuck3 = document.getElementById("fuck3")
     let fuck4 = document.getElementById("fuck4")
-    fuck.innerHTML = 'Hello World!'
     fuck1.innerHTML = 'Hello World!'
     fuck2.innerHTML = 'Hello World!'
     fuck3.innerHTML = 'Hello World!'
