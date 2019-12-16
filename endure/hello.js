156919142031941f51c746b45bdeccd8142b9f1c
Mark <clashclanacc2602@gmail.com>
2019-12-16 21:01:29 +0200

    forgot script!

--- endure/hello.js
+++ endure/hello.js
@@ -1,5 +1,13 @@
 function world()
 {
-    let lastfuck = document.getElementById('fuckinglast');
-    lastfuck.innerHTML = 'Hello World!'
+    let fuck = document.getElementById("fuck")
+    let fuck1 = document.getElementById("fuck1")
+    let fuck2 = document.getElementById("fuck2")
+    let fuck3 = document.getElementById("fuck3")
+    let fuck4 = document.getElementById("fuck4")
+    fuck.innerHTML = 'Hello World!'
+    fuck1.innerHTML = 'Hello World!'
+    fuck2.innerHTML = 'Hello World!'
+    fuck3.innerHTML = 'Hello World!'
+    fuck4.innerHTML = 'Hello World!'
 }
